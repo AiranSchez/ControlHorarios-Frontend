c8a44055518c60d5846dcdcc63fe4f4eba946262
AiranSchez <airanschez0@gmail.com>
2020-12-03 17:15:01 +0000

    Added assets for Homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,30 @@
 <template>
-  <div class="home flex flex-col h-screen ">
+  <div class="home flex flex-col min-h-screen ">
+    <img src="../assets/Home-picture-1.jpg" alt="Main picture"/>
+    <div class="bg-white flex flex-col items-center justify-around pt-10 pb-10">
+      <div class="font-bold text-xl pb-4 transform">Controla tus horas totales</div>
+      <div class="text-base flex flex-col items-center pb-4">
+        <div>Nuestra meta es lograr que cada día</div>
+        <div>sea más fácil para ti trabajar</div>
+      </div>
+      <img src="../assets/Home-icon-1.png" alt="Calendar icon" width="100px" height="100px">
+    </div>
+    <img src="../assets/Home-picture-2.jpg" alt="Main picture"/>
+    <div class="bg-white flex flex-col items-center justify-around pt-10 pb-10 ">
+      <div class="font-bold text-xl pb-4">Organiza a tu equipo</div>
+      <div class="text-base flex flex-col items-center pb-4">
+        <div>Gestiona las vacaciones individuales</div>
+        <div>como nunca lo has hecho</div>
+      </div>
+      <img src="../assets/Home-icon-2.png" alt="Chronometer icon" width="100px" height="100px">
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'Home',
   components: {
-
   }
 }
 </script>
