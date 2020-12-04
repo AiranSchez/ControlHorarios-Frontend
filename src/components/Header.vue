2594f364905fd5a5745a53e91594399dc32210f3
AiranSchez <airanschez0@gmail.com>
2020-12-04 01:00:27 +0000

    :fire: Small changes in views and components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,12 @@
 <template>
-  <div id="header">
-    <div id="nav">
-      <div class="links">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>|
+  <div id="header" class="flex">
+    <div id="nav" class="absolute bg-gray-100 bg-opacity-20 flex w-full  items-center justify-between flex-wrap p-2 m-auto top-0 animated">
+      <div class="text-black">Logo</div>
+      <div class="links flex text-lg font-bold text-white w-2/4 justify-between">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">Products</router-link>
+        <router-link to="/about">FAQ</router-link>
+        <router-link to="/login">Sign in</router-link>
         <router-link to="/register">Register</router-link>
       </div>
     </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,17 +7,18 @@
         <div>Nuestra meta es lograr que cada día</div>
         <div>sea más fácil para ti trabajar</div>
       </div>
-      <img src="../assets/Home-icon-1.png" alt="Calendar icon" width="100px" height="100px">
+      <img src="../assets/Home-icon-1.png" alt="Calendar icon" class="w-20 h-20 shadow-2xl">
     </div>
-    <img src="../assets/Home-picture-2.jpg" alt="Main picture"/>
+    <img src="../assets/Home-picture-2.jpg" alt="Calendar picture"/>
     <div class="bg-white flex flex-col items-center justify-around pt-10 pb-10 ">
       <div class="font-bold text-xl pb-4">Organiza a tu equipo</div>
       <div class="text-base flex flex-col items-center pb-4">
         <div>Gestiona las vacaciones individuales</div>
         <div>como nunca lo has hecho</div>
       </div>
-      <img src="../assets/Home-icon-2.png" alt="Chronometer icon" width="100px" height="100px">
+      <img src="../assets/Home-icon-2.png" alt="Chronometer icon" class="w-20 h-20 ">
     </div>
+    <img src="../assets/Home-picture-3.jpg" alt="Meeting Picture"/>
   </div>
 </template>
 
