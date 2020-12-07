8fa065322cfa7c8601d5e73aea66e0fb4312f49a
AiranSchez <airanschez0@gmail.com>
2020-12-07 18:32:26 +0000

    :sparkles: Added basic API calls

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,9 +2,8 @@
   <div id="header" class="flex">
     <div id="nav" class="absolute bg-gray-100 bg-opacity-20 flex w-full  items-center justify-between flex-wrap p-2 m-auto top-0 animated">
       <div class="text-black">Logo</div>
-      <div class="links flex text-lg font-bold w-2/4 justify-between">
+      <div class="links flex text-lg font-bold w-1/4 justify-between">
         <router-link class="text-white hover:text-gray-500" to="/">Home</router-link>
-        <router-link class="text-white hover:text-gray-500" to="/about">Products</router-link>
         <router-link class="text-white hover:text-gray-500" to="/about">FAQ</router-link>
         <router-link class="text-white hover:text-gray-500" to="/login">Sign in</router-link>
         <router-link class="text-white hover:text-gray-500" to="/register">Register</router-link>
