04447c501ddecbf0115b82d3ba087ddf80094b38
arianezf <arianezf@gmail.com>
2020-12-08 19:39:21 +0000

    :sparkles: Creating company profile

---
+++ src/components/CompanyProfile/Employees/EmployeeForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <h1 class="mb-8 text-3xl text-center">Sign Up ;)</h1>
+            <FormInput label="Name" placeholder="Your name" v-on:fieldValue="nameReceived"/>
+            <FormInput label="Email" placeholder="Email" v-on:fieldValue="emailReceived"/>
+            <FormInput label="Password" placeholder="Your password" v-on:fieldValue="passwordReceived"/>
+            <FormInput label="Confirm password" placeholder="Your password" v-on:fieldValue="repeatedPasswordReceived"/>
+            <FormInput label="Company name" placeholder="Your company name" v-on:fieldValue="companyNameReceived"/>
+            <FormInput label="Location" placeholder="Your company location" v-on:fieldValue="locationReceived"/>
+            <FormButton @click.native="register" value="Sign up"/>
+  </div>
+</template>
+
+<script>
+import FormInput from '../Forms/FormInput'
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,6 @@ module.exports = {
     }
   },
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {}
-  },
   variants: {
     extend: {}
   },
