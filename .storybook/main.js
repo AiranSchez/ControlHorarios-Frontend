745ba297bb513d65fc64d43a01c68211d7f6c2e1
AiranSchez <airanschez0@gmail.com>
2020-12-10 17:40:01 +0000

    :construction: Installing storybook

---
+++ .storybook/main.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-actions",
+    "@storybook/addon-a11y"
+  ]
+}
+const path = require("path")
+
+module.exports = {
+  webpackFinal: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.join(__dirname, "../src/")
+    }
+    return config
+  }
+}
