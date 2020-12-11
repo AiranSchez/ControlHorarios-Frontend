9e7a7b83ef4981389dcce7dd1f59d3b242c6dffe
AiranSchez <airanschez0@gmail.com>
2020-12-11 19:48:25 +0000

    :ambulance: Fixed dynamic password check

--- src/components/Forms/FormInputPassword/FormInputPassword.vue
+++ src/components/Forms/FormInputPassword/FormInputPassword.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="mb-4">
     <label :for="id" class="font-bold text-grey-darker block mb-2">{{ label }}</label>
-    <input :id="id" type="password" @change="objChanged()" class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow" :placeholder="placeholder" v-model="field">
-    <div v-if="this.isValidPassword">Valid password</div>
+    <input :id="id" type="password" @change="checkPassword" class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow" :placeholder="placeholder" v-model="field">
+    <div v-if="checkPattern(this.field)">Valid password</div>
     <div v-else>Invalid password</div>
   </div>
 </template>
@@ -17,22 +17,20 @@ export default {
   },
   data () {
     return {
-      field: '',
-      isValidPassword: false
+      field: ''
     }
   },
   methods: {
-    objChanged () {
-      const regExp = /^[a-zA-Z0-9]{8,16}$/
-      if (regExp.test(this.field)) {
-        this.isValidPassword = true
-        console.log(this.isValidPassword)
+    checkPassword () {
+      if (this.checkPattern(this.field)) {
         this.$emit('fieldValue', this.field)
       } else {
-        this.isValidPassword = false
-        console.log(this.isValidPassword)
         console.log('Incorrect password')
       }
+    },
+    checkPattern (pass) {
+      const regExp = /^[a-zA-Z0-9]{8,16}$/
+      return regExp.test(pass)
     }
   }
 }
