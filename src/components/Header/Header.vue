8858cbf8c3e6125862c72775077fe0b9531c1cc5
Manuel Pérez Regalado <perezregaladomanuel@gmail.com>
2020-12-11 19:05:51 +0000

    ✨ Refactor Header

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="header" class="flex">
-    <div id="nav" class="absolute bg-gray-100 bg-opacity-20 flex w-full  items-center justify-between flex-wrap p-2 m-auto top-0 animated">
-      <div class="text-black">Logo</div>
-      <div class="links flex text-lg font-bold w-1/4 justify-between">
-        <router-link class="text-white hover:text-gray-500" to="/">Home</router-link>
-        <router-link class="text-white hover:text-gray-500" to="/about">FAQ</router-link>
-        <router-link class="text-white hover:text-gray-500" to="/login">Sign in</router-link>
-        <router-link class="text-white hover:text-gray-500" to="/register">Register</router-link>
+    <div id="nav" class="bg-white w-full h-20 flex items-center justify-around flex-wrap p-2 m-auto top-0 border-b-2 border-gray-300">
+      <div class="text-black"><img src="../../assets/kiwi.png" alt="Logo"></div>
+      <div class="links flex text-lg w-1/4 justify-evenly text-lg">
+        <router-link class="hover:text-gray-500" to="/">Home</router-link>
+        <router-link class="hover:text-gray-500" to="/about">FAQ</router-link>
+        <div v-if="!isUserLogged()" class="links flex text-lg w-1/2 justify-evenly text-lg" >
+          <router-link class="hover:text-gray-500" to="/login">Sign in</router-link>
+          <router-link class="border border-green-400 rounded-full h-8 w-24 text-center hover:text-gray-500 hover:bg-green-400" to="/register">Sign up</router-link>
+        </div>
+        <div v-else></div>
       </div>
     </div>
   </div>
@@ -14,9 +17,15 @@
 
 <script>
 export default {
+  name: 'FormInput',
+  props: {
+  },
+  data () {
+  },
+  methods: {
+    isUserLogged () {
+      return localStorage.getItem('UserID')
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
