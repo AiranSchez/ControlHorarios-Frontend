92acd007c774d393ba6d38bc2204ef410a6f2016
AiranSchez <airanschez0@gmail.com>
2020-12-11 22:17:33 +0000

    :recycle: Fixed messages and feedback in RegisterForm

--- src/components/Forms/FormInputPassword/FormInputPassword.vue
+++ src/components/Forms/FormInputPassword/FormInputPassword.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mb-4">
     <label :for="id" class="font-bold text-grey-darker block mb-2">{{ label }}</label>
-    <input :id="id" type="password" @change="checkPassword" :class="[ isValidPassword && field !== '' ? validStyles : invalidStyles ]" class="block appearance-none w-full bg-white hover:border-grey px-2 py-2 rounded shadow" :placeholder="placeholder" v-model="field">
+    <input :id="id" type="password" @change="checkPassword" :class="[ field !== '' ? [isValidPassword ? validStyles : invalidStyles] : defaultStyles ]" class="block appearance-none w-full bg-white hover:border-grey px-2 py-2 rounded shadow" :placeholder="placeholder" v-model="field">
     <div v-if="!checkPattern(this.field)">Invalid password</div>
   </div>
 </template>
@@ -19,7 +19,8 @@ export default {
       field: '',
       isValidPassword: '',
       validStyles: 'border-2 border-green-300',
-      invalidStyles: 'border-2 border-red-300'
+      invalidStyles: 'border-2 border-red-300',
+      defaultStyles: 'border-2 border-grey-300'
     }
   },
   methods: {
