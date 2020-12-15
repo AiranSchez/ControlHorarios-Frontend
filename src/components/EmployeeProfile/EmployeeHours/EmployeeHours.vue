4fca0bec50c31ec666199d2e8c4fd69cec22ab7e
AiranSchez <airanschez0@gmail.com>
2020-12-15 14:20:03 +0000

    :sparkles: Added basic summary for employee

---
+++ src/components/EmployeeProfile/EmployeeHours/EmployeeHours.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div v-for="record in records" :key="record">
+      <div class="flex ">
+        <p>Check in: {{record.StartTime}} -</p>
+        <p>Check out: {{record.EndTime}}</p>
+      </div>
+    </div>
+    <div v-for="hour in hours" :key="hour">
+      <div class="flex">
+        <div>{{hour}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getSummary } from '@/domain/services/employeeServices'
+import dayjs from 'dayjs'
+
+export default {
+  name: 'EmployeeHours',
+  mounted () {
+    const employeeID = localStorage.getItem('EmployeeID')
+    getSummary(employeeID)
+      .then(resp => {
+        resp.forEach(record => {
+          this.records.push(record)
+        })
+        this.calculateDayHours()
+      })
+  },
+  methods: {
+    calculateDayHours () {
+      this.records.forEach(record => {
+        const today = dayjs().format('YYYY-MM-DD')
+        if (dayjs(record.StartTime).format('YYYY-MM-DD') === today) {
+          const StartTime = dayjs(record.StartTime)
+          const EndTime = dayjs(record.EndTime)
+          const difference = dayjs(EndTime.diff(StartTime)).format('HH:mm:ss')
+          this.hours.push(difference)
+        }
+      })
+    }
+  },
+  data () {
+    return {
+      records: [],
+      hours: []
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
